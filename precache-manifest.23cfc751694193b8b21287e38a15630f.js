a722be3c95459508fbfa5ca7adfa5a17da827654
AYUSH SONI <ayushakshay.soni@gmail.com>
2020-05-10 15:06:56 +0000

    Updates

---
+++ precache-manifest.23cfc751694193b8b21287e38a15630f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee541ba530401c29a26d3097a810230",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "d258f7b7d8f8bda395f5",
+    "url": "/Recipe/static/css/main.ea18ae33.chunk.css"
+  },
+  {
+    "revision": "f6425d9f91679860f814",
+    "url": "/Recipe/static/js/2.06ec4b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe/static/js/2.06ec4b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d258f7b7d8f8bda395f5",
+    "url": "/Recipe/static/js/main.bfc44d63.chunk.js"
+  },
+  {
+    "revision": "9d08646faeae34f4cd68",
+    "url": "/Recipe/static/js/runtime-main.7672d406.js"
+  }
+]);
